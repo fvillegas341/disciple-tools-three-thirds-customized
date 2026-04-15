7e65ec26b0f4df93a8bc04346ed7f4cea9075000
fvillegas341 <frankvillegas808@gmail.com>
2026-04-14 19:09:29 -0700

    add group field in createmeeting page

--- magic-link/pages/CreateMeeting.jsx
+++ magic-link/pages/CreateMeeting.jsx
@@ -1,7 +1,8 @@
-import React, {Fragment, useCallback, useContext, useEffect, useState} from 'react'
+import React, { Fragment, useCallback, useContext, useEffect, useState } from 'react'
 import Card from "../components/layout/cards/Card";
-import {Button, ButtonGroup, TabPanel, TabsContent} from "react-foundation";
+import { Button, ButtonGroup, TabPanel, TabsContent } from "react-foundation";
 import AppContext from "../contexts/AppContext";
+import MeetingContext from "../contexts/MeetingContext";
 import CardHeading from "../components/layout/cards/CardHeading";
 import CardSection from "../components/layout/cards/CardSection";
 import Form from "../components/forms/Form";
@@ -9,8 +10,9 @@ import FieldGroup from "../components/forms/FieldGroup";
 import ApplicationLayout from "../layouts/ApplicationLayout";
 import CardFooter from "../components/layout/cards/CardFooter";
 import DateField from "../components/forms/DateField";
-import {createMeeting} from '../src/api'
-import {useNavigate} from "react-router-dom";
+import CreatableRelationshipField from "../components/forms/CreatableRelationshipField";
+import { createMeeting, searchGroups } from '../src/api';
+import { useNavigate } from "react-router-dom";
 
 /**
  * The create meeting page
@@ -18,18 +20,19 @@ import {useNavigate} from "react-router-dom";
  * @constructor
  */
 const CreateMeetingPage = () => {
-    const {translations} = useContext(AppContext)
+    const { translations } = useContext(AppContext)
+    const { meeting } = useContext(MeetingContext)
     let navigate = useNavigate();
 
 
     return (
         <ApplicationLayout title={translations.create_meeting}
-                           breadcrumbs={[
-                               {
-                                   link: '/',
-                                   label: 'Dashboard'
-                               }
-                           ]}>
+            breadcrumbs={[
+                {
+                    link: '/',
+                    label: 'Dashboard'
+                }
+            ]}>
             <Form
                 request={createMeeting}
                 initialValues={{
@@ -52,7 +55,7 @@ const CreateMeetingPage = () => {
                     }
                 }}
             >
-                {({values, isSubmitting, setFieldValue, setTouched, ...attrs}) => {
+                {({ values, isSubmitting, setFieldValue, setTouched, ...attrs }) => {
                     return <Fragment>
                         <main>
                             <div className={"container"}>
@@ -72,6 +75,12 @@ const CreateMeetingPage = () => {
                                             name="date"
                                             placeholder={translations.date}
                                             component={DateField}
+                                        />
+                                        <FieldGroup name="groups"
+                                            request={searchGroups}
+                                            defaultValue={meeting.groups?.posts}
+                                            component={CreatableRelationshipField}
+                                            isMulti
                                         />
                                     </CardSection>
                                     <CardFooter>
